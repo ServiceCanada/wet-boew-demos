dd6e1a586175cca41c71f9c7e1cc09d179d0b692
Marc-André Garneau <marcandre.garneau@servicecanada.gc.ca>
2024-05-24 15:00:19 -0400

    Building GCWeb PR 2371 - Homepage banner - as of 2024/05/24

--- méli-mélo-demos/2024-09-kejimkujik/2024-09-kejimkujik.js
+++ méli-mélo-demos/2024-09-kejimkujik/2024-09-kejimkujik.js
@@ -71,10 +71,16 @@
 
                 var megamenuAjaxReplace = $elm[0].getAttribute('data-megamenu-ajax');
 
+				var megamenuColorClass = "";
+                
+                if ($elm[0].hasAttribute('data-megamenu-bg-color')) {
+                    megamenuColorClass = $elm[0].getAttribute('data-megamenu-bg-color');
+                }
+
                 // Wrap menu HTML with the megamenu wrapper
                 // NOTE: Removed role="navigation" (redundant) and typeof="SiteNavigationElement" (not required)
                 megamenuHTML = `
-                <nav id="wb-sm" class="campaign-menu wb-menu visible-md visible-lg" data-trgt="mb-pnl" data-ajax-replace="${megamenuAjaxReplace}">
+                <nav id="wb-sm" class="campaign-menu wb-menu visible-md visible-lg ${megamenuColorClass}" data-trgt="mb-pnl" data-ajax-replace="${megamenuAjaxReplace}">
                     <div class="pnl-strt nvbar">
                         <h2>${gcwebMenuH2.textContent}</h2>
                         <ul role="menubar" class="list-inline menu">
@@ -1155,3 +1161,75 @@ $document.on( "timerpoke.wb " + initEvent, selector, init );
 // Add the timer poke to initialize the plugin
 wb.add( selector );
 } )( jQuery, window, wb );
+
+/**
+ * @title WET-BOEW Steps Quiz plugin
+ * @overview Quiz pattern for use with the Steps Form plugin.
+ * @license wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
+ * @author ricokola
+ */
+( function( $, document ) {
+"use strict";
+
+//Detect the enhancement of the quiz
+var quizSelector = ".provisional.wb-steps.quiz",
+	instances = document.querySelectorAll( quizSelector );
+
+//How many quiz instances in the page
+instances.forEach ( ( instance ) => {
+	let $instance = $( instance );
+
+	// Calculate number of questions
+	let numQuestion = $( "fieldset", $instance ).length;
+
+	// Addition to UI (Ex: progress bar)
+	$( "form", $instance ).prepend( "<p class='progressText' role='status'></p>" );
+	$( "form", $instance ).prepend( "<p><progress class='progressBar' max='" + numQuestion + "'></progress></p>" );
+
+});
+
+var hideOtherSteps = function( e ) {
+	// Get wb-steps component
+	let steps,
+	currentElement = e.currentTarget;
+
+	if ( currentElement.classList.contains( "quiz" ) && currentElement.classList.contains( "wb-steps" ) ) {
+		steps = currentElement;
+
+	} else {
+		steps = $( currentElement ).parentsUntil( quizSelector ).parent().get( 0 );
+	}
+
+
+	// Check if the instance is not found
+	if ( !steps || steps instanceof HTMLDocument ) {
+		return;
+	}
+
+	// Find the steps form context and validate it is a quiz
+	let currentTabId = $( "legend.wb-steps-active:first-child", steps ).parents().prevAll( ".steps-wrapper" ).length + 1;
+	
+	// Get progress bar
+	let $progressBar = $( ".progressBar", steps );
+
+	// Get number of questions
+	let numQuestion = $progressBar.attr( "max" );
+
+	// Set the progress label
+	$( "p.progressText", steps ).text( currentTabId + " of  " + numQuestion );
+
+	// Update progress bar
+  	$progressBar.val( currentTabId );
+
+  	// Hide other steps that are not active
+	$( ".steps-wrapper", steps ).removeClass( "hidden" );
+	$( ".steps-wrapper:has( div.hidden )", steps ).addClass( "hidden" );
+
+};
+
+$( document ).on( "click", quizSelector + " .steps-wrapper div.buttons > :button", hideOtherSteps );
+
+//Init
+$( quizSelector ).on( "wb-ready.wb-steps", hideOtherSteps );
+
+} )( jQuery, document );
