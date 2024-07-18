b0db3004c2225a8f0eac4dd0bae187aca3564bc5
Marc-André Garneau <marcandre.garneau@servicecanada.gc.ca>
2024-07-18 08:47:49 -0400

    Building Theme template demo - as of 2024/07/18

--- dist/GCWeb/js/theme.js
+++ dist/GCWeb/js/theme.js
@@ -1,7 +1,7 @@
 /*!
  * @title Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
  * @license wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
- * v15.3.0 - 2024-07-15
+ * v15.3.0 - 2024-07-18
  *
  */( function( $, document, wb ) {
 "use strict";
@@ -4179,3 +4179,62 @@ if ( document.querySelector( ".gc-minister" ) && !document.body.classList.contai
 	document.body.classList.add( "page-type-ilp" );
 	console.warn( "It seems that this page is an institutional landing page. However, the <body> element is missing the \"page-type-ilp\" CSS class. It has been added for your convenience, but please make sure you follow the technical guidance: https://wet-boew.github.io/GCWeb/templates/institutional-landing/institutional-landing-doc-en.html" );
 }
+
+( function( $, window, wb ) {
+"use strict";
+
+var $document = wb.doc,
+	componentName = "page-type-theme",
+	selector = "." + componentName,
+	initEvent = "wb-init " + selector,
+
+	/**
+	 * @method init
+	 * @param {jQuery Event} event Event that triggered the function call
+	 */
+	init = function( event ) {
+
+		// Start initialization
+		// returns DOM object = proceed with init
+		// returns undefined = do not proceed with init (e.g., already initialized)
+		var elm = wb.init( event, componentName, selector );
+
+		if ( elm && event.currentTarget === event.target ) {
+
+			let themeMenuBtn = document.querySelector( "#menuTrigger" ),
+				themeNavUL = document.querySelector( "#gridContainer > nav ul" ),
+				activePageLink = themeNavUL.querySelector( ".active a" );
+
+			themeNavUL.id = themeNavUL.id || wb.getId();
+			activePageLink.setAttribute( "aria-current", "page" );
+			themeMenuBtn.setAttribute( "aria-controls", themeNavUL.id );
+			themeMenuBtn.setAttribute( "aria-expanded", "false" );
+
+			// Identify that initialization has completed
+			wb.ready( $( elm ), componentName );
+		}
+	};
+
+// Bind the init event of the plugin
+$document.on( "timerpoke.wb " + initEvent, selector, init );
+
+// On click of the menu button
+$document.on( "click", "#menuTrigger", function( event ) {
+	let themeMenuBtn = event.currentTarget,
+		themeNavUL = document.querySelector( "#" + themeMenuBtn.attributes[ "aria-controls" ].value );
+
+	if ( themeMenuBtn.getAttribute( "aria-expanded" ) === "true" ) {
+		themeNavUL.classList.remove( "visible-xs", "visible-sm" );
+		themeMenuBtn.setAttribute( "aria-expanded", "false" );
+		themeMenuBtn.classList.remove( "expanded" );
+	} else {
+		themeNavUL.classList.add( "visible-xs", "visible-sm" );
+		themeMenuBtn.setAttribute( "aria-expanded", "true" );
+		themeMenuBtn.classList.add( "expanded" );
+	}
+} );
+
+// Add the timer poke to initialize the plugin
+wb.add( selector );
+
+} )( jQuery, window, wb );
